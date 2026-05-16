d50ad37317acfa120d880bb979322a06629a6211
TS-DEV-DEBUG-V2 <finishmealready8@gmail.com>
2026-05-16 19:13:55 +0300

    Add files via upload

---
+++ src/psx/rasteriser/colour.rs
@@ -0,0 +1,66 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, Deserialize, Serialize)]
+pub struct Colour {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: bool,
+}
+
+impl Colour {
+    #[inline(always)]
+    pub fn new(r: u8, g: u8, b: u8, a: bool) -> Colour {
+        Colour { r, g, b, a }
+    }
+
+    #[inline(always)]
+    pub fn from_u16(colour: u16) -> Colour {
+        let rb = (colour & 0x1f) as u8;
+        let gb = ((colour >> 5) & 0x1f) as u8;
+        let bb = ((colour >> 10) & 0x1f) as u8;
+
+        let r = (rb << 3) | (rb >> 2);
+        let g = (gb << 3) | (gb >> 2);
+        let b = (bb << 3) | (bb >> 2);
+        let a = (colour >> 15) != 0;
+
+        Colour::new(r, g, b, a)
+    }
+
+    #[inline(always)]
+    pub fn from_u32(colour: u32) -> Colour {
+        let r = colour as u8;
+        let g = (colour >> 8) as u8;
+        let b = (colour >> 16) as u8;
+
+        Colour::new(r, g, b, false)
+    }
+
+    #[inline(always)]
+    pub fn to_u16(self) -> u16 {
+        let mut pixel = 0;
+
+        pixel |= ((self.r as u16) & 0xf8) >> 3;
+        pixel |= ((self.g as u16) & 0xf8) << 2;
+        pixel |= ((self.b as u16) & 0xf8) << 7;
+        pixel |= (self.a as u16) << 15;
+
+        pixel
+    }
+
+    #[inline(always)]
+    pub fn r(self) -> i32 {
+        self.r as i32
+    }
+
+    #[inline(always)]
+    pub fn g(self) -> i32 {
+        self.g as i32
+    }
+
+    #[inline(always)]
+    pub fn b(self) -> i32 {
+        self.b as i32
+    }
+}
---
+++ src/psx/rasteriser/vector3i.rs
@@ -0,0 +1,13 @@
+#[derive(Clone, Copy)]
+pub struct Vector3i {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+impl Vector3i {
+    #[inline(always)]
+    pub fn new(x: i32, y: i32, z: i32) -> Vector3i {
+        Vector3i { x, y, z }
+    }
+}
